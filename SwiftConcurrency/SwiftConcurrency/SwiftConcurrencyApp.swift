d1c048051c33bf71c6b12ef8284ce19f865f294b
Sreekutty Maya <sreekuttystar@gmail.com>
2025-02-01 14:57:57 +0400

    project setup

---
+++ SwiftConcurrency/SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Sreekutty Maya on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
