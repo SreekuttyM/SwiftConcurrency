422d6e6426b91e19a33013b88fc80209e38ab139
Sreekutty Maya <sreekuttystar@gmail.com>
2025-02-02 14:01:07 +0400

    different thread understanding with dispatchqueue and async

---
+++ SwiftConcurrency/SwiftConcurrency/AsyncAwait.swift
@@ -0,0 +1,62 @@
+//
+//  AsyncAwait.swift
+//  SwiftConcurrency
+//
+//  Created by Sreekutty Maya on 02/02/2025.
+//
+
+import SwiftUI
+
+class AsyncAwaitViewModel : ObservableObject {
+    @Published var data : [String] = []
+    
+    func addItem() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.data.append("\(Thread.current)")
+        }
+        addItem2()
+    }
+    
+    func addItem2() {
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            let title = "\(Thread.current)"
+            DispatchQueue.main.async {
+                self.data.append(title)
+            }
+        }
+    }
+    
+    func addAuthor1() async{
+        let author1 =  "\(Thread.current)"
+        self.data.append(author1)
+        try? await Task.sleep(nanoseconds: 2_00_000_000)
+        let author2 =  "\(Thread.current)"
+        
+        await MainActor.run {
+            self.data.append(author2)
+            let author3 =  "\(Thread.current)"
+            self.data.append(author3)
+        }
+    }
+}
+
+struct AsyncAwait: View {
+     @ObservedObject var viewModel = AsyncAwaitViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.data,id:\.self) { data in
+                Text("\(data)")
+            }
+        }.onAppear() {
+            Task {
+              await  viewModel.addAuthor1()
+            }
+        }
+    }
+}
+
+#Preview {
+    AsyncAwait()
+}
